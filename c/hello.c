2ca7a4878f1d6a229215232d497f5f6ac5d106f6
joashgobin <joashgobin@gmail.com>
2023-12-23 14:24:11 -0400

    Tweaks circa Sat Dec 23 14:14:19 -04 2023

---
+++ c/hello.c
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html><html lang='en'>
+<head>
+<meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Code Snippets</title>
+    <link rel='stylesheet' href='/highlight/styles/base16/ros-pine.css'>
+</head>
+<body style='background-color:black'>
+<small style='color:darkgray'>c</small>
+<h2 style='color:orange'>hello.c</h2>
+<pre>
+<code class='c'>
+#include &lt;stdio.h&gt;
+
+int main(){
+    printf(&quot;Hello world!&quot;);
+    return 0;
+}
+
+
+</code>
+</pre>
+<script src='/highlight/highlight.min.js'></script>
+<script src='/highlight/languages/c.min.js'></script>
+<script>
+    hljs.highlightAll();
+</script>
+</body>
+</html>
